7cdc3e8015ee7105ebb664d894ff5235931bd215
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-03-03 19:48:15 +0000

    Add a proof of concept IRC connection and file logging

--- logbot.py
+++ logbot.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import time
 import signal
+import irc.client
 
 
 def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
@@ -50,12 +50,23 @@ def main():
     sys.stdout.write('Daemon started with pid %d\n' % os.getpid())
     sys.stdout.write('Daemon stdout output\n')
     sys.stderr.write('Daemon stderr output\n')
-    c = 0
-    while 1:
-        sys.stdout.write('%d: %s\n' % (c, time.ctime(time.time())))
-        sys.stdout.flush()
-        c = c + 1
-        time.sleep(1)
+    irciffy()
+
+
+def f(c, e):
+    sys.stdout.write(e.arguments[0])
+    sys.stdout.flush()
+
+
+def irciffy():
+    client = irc.client.IRC()
+    c = client.server().connect('irc.freenode.net', 6667, 'op_bot')
+    c.join("#openpassword")
+    c.add_global_handler('pubmsg', f)
+    c.add_global_handler('privmsg', f)
+
+    client.process_forever()
+
 
 if __name__ == "__main__":
     daemonize('/dev/null', '/tmp/daemon.log', '/tmp/daemon.log')
